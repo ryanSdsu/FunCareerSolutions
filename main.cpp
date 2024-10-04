ed69a302b8e32930c720552b9e79bdecd2198644
Ryan <ryan.ragasa.sdsu@gmail.com>
2024-10-04 15:11:17 +0000

    Updated the files to talk about preprocessor statements.

--- main.cpp
+++ main.cpp
@@ -5,5 +5,5 @@ int main() {
   std::cout << "Hello World! This is the start of a new journey in Fun Career "
                "Solutions! \n";
 
-  std::cout << "Fun fact to remember is that the # when placed in the beginning of a line indicates that it's a preprocesor statement. So the header file is included into our program. \n";
+  std::cout << "Fun fact to remember is that the # when placed in the beginning of a line indicates that it's a preprocesor statement. So the header file is included into our program. Or anything else. \n";
 }
